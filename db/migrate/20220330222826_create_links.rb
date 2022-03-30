309268d1c6c1435465ff835d786f9d43553e6b0a
Amir <iskandar.amir@gmail.com>
2022-03-31 06:39:22 +0800

    Generate Links model migration

---
+++ db/migrate/20220330222826_create_links.rb
@@ -0,0 +1,12 @@
+class CreateLinks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :links do |t|
+      t.string :title
+      t.string :href
+      t.integer :clicks
+      # Add hash
+
+      t.timestamps
+    end
+  end
+end
