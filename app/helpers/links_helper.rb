d1c569f5487b4a33294c7c8d8b1ce116ed5a9c9a
Amir <iskandar.amir@gmail.com>
2022-04-01 01:02:54 +0800

    Title grabber

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,9 @@
+require 'nokogiri'
+require 'open-uri'
+
 module LinksHelper
+    def get_page_title(href)
+        doc = Nokogiri::HTML(URI.open(href))
+        doc.at_css('title').text
+    end
 end
