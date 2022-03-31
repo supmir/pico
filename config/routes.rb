9019ad2807a932175db2c1a7fececa0e12337f31
Amir <iskandar.amir@gmail.com>
2022-03-31 19:39:50 +0800

    Link name correction

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "shortenedlinks#new"
+  root "shortened_links#new"
 
   # get "/links", to: "links#index"
   # get "/links/:id", to: "links#show"
