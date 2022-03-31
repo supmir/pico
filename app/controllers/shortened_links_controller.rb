b8d8936fdab6c6a9f8dcd4e8ac61ca5d372218df
Amir <iskandar.amir@gmail.com>
2022-04-01 01:07:13 +0800

    Shortener complete

--- app/controllers/shortened_links_controller.rb
+++ app/controllers/shortened_links_controller.rb
@@ -1,4 +1,34 @@
+require 'uri'
 class ShortenedLinksController < ApplicationController
   def new
+    @shortened_link = ShortenedLink.new
+  end
+
+  def create
+    @href = params[:shortened_link][:href]
+    if !(@href =~ /^https?:\/\//)
+      @href = 'http://' + @href
+    end
+    @link = Link.find_by_href(@href)
+    if !@link
+      @link = Link.create_new_using_href(@href)
+      if !@link.save
+        render :new, status: :unprocessable_entity
+      end
+    end
+    loop do
+      @path = helpers.generate_new_name
+      shortened_link = ShortenedLink.find_by_path(@path)
+      if !@shortened_link
+        break
+      end
+    end
+    @shortened_link = ShortenedLink.new(
+        path:@path,
+        link:@link
+    )
+    if !@shortened_link.save
+      render :new, status: :unprocessable_entity
+    end
   end
 end
