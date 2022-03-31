7227fac0ce4983e2befe090ab0c135c7b059ed4c
Amir <iskandar.amir@gmail.com>
2022-04-01 01:04:58 +0800

    Custom new function for Links model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,8 @@
 class Link < ApplicationRecord
     has_many :shortened_links
+
+    def self.create_new_using_href(href)
+      @title = ApplicationController.helpers.get_page_title(href)
+      Link.new(title:@title, href:href)
+    end
 end
