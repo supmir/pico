928942a5b57e08c9a27137dcdf037bd41017e7dd
Amir <iskandar.amir@gmail.com>
2022-04-01 23:19:45 +0800

    HTTP HTTPS handling

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -3,7 +3,22 @@ require 'open-uri'
 
 module LinksHelper
     def get_page_title(href)
-        doc = Nokogiri::HTML(URI.open(href))
+        begin
+            data = URI.open(href)
+            doc = Nokogiri::HTML(data)
+        rescue
+            href = self.switch_protocol(href)
+            data = URI.open(href)
+            doc = Nokogiri::HTML(data)
+        end
         doc.at_css('title').text
+    end
+    def switch_protocol(href)
+        if href =~ /^https:\/\//
+            href[0..7] = 'http://'
+        elsif href =~ /^http:\/\//
+            href[0..6] = 'https://'
+        end
+        href
     end
 end
