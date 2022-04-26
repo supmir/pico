d95df05fae2906ca87d81849727635f2b0244c8d
Amir <iskandar.amir@gmail.com>
2022-04-27 03:33:57 +0800

    Geocode fix

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -1,7 +1,11 @@
 class Analytic < ApplicationRecord
   belongs_to :shortened_link
   geocoded_by :ip,
-    :latitude => :lat, :longitude => :lon
-  reverse_geocoded_by :lat, :lon
+    :latitude => :lat, :longitude => :lon, :city => :city, :country_code=> :country
+  # reverse_geocoded_by :lat, :lon do |obj,results|
+  #   if geo = results.first
+  #     obj.city    = geo.city
+  #     obj.country = geo.country_code
+  #   end
   after_validation :geocode, :reverse_geocode
 end
