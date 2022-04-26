0751a6518f0aba481ff87f9ecd53d444309357bf
Amir <iskandar.amir@gmail.com>
2022-04-27 03:24:10 +0800

    Geocode fix

--- app/models/analytic.rb
+++ app/models/analytic.rb
@@ -2,5 +2,6 @@ class Analytic < ApplicationRecord
   belongs_to :shortened_link
   geocoded_by :ip,
     :latitude => :lat, :longitude => :lon
-  after_validation :geocode
+  reverse_geocoded_by :lat, :lon
+  after_validation :geocode, :reverse_geocode
 end
